11cb903c7f15b3888d88d2af279f2b0d1107d5f3
Ryo Nakamaru <pottava@gmail.com>
2017-03-10 11:02:39 +0900

    dev environment
    Signed-off-by: Ryo Nakamaru <pottava@gmail.com>

---
+++ development/generate-data.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+WKDIR=$(pwd)
+if [ ! -d "${WKDIR}/.git" ]; then
+  echo 'This script must be executed on local git repository root dir.' 1>&2
+  exit 1
+fi
+
+pushd data
+rm -rf ./*.txt
+find . -name "*.pdf" -exec pdftotext -enc ASCII7 {} \;
+popd
+
+cat ./data/*.txt | tr '\n' '.' | tr '\r' '.' \
+  | sed -e 's/\.\.*/. /g' -e 's/[[:blank:]][[:blank:]]*/ /g' \
+  | fold -w 125 -s > input.txt
