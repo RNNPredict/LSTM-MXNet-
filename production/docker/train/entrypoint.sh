fd262ac067c469fa4388e92e9058bd550a04a378
Ryo Nakamaru <pottava@gmail.com>
2017-03-11 01:43:23 +0900

    dockerized training job

---
+++ production/docker/train/entrypoint.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  echo "Missing environment variable 'AWS_DEFAULT_REGION'." 1>&2
+  exit 1
+fi
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "Missing environment variable 'AWS_S3_BUCKET'." 1>&2
+  exit 1
+fi
+if [ -z "$LEARNING_EPOCHS" ]; then
+  echo "Missing environment variable 'LEARNING_EPOCHS'." 1>&2
+  exit 1
+fi
+if [ -z "$AWS_S3_PREFIX" ]; then
+  AWS_S3_PREFIX="$(date +%M%H%d%m%Y)"
+fi
+
+export VOCABULARY_FILE="./input.txt"
+export PARAMETERS_FILE="model"
+AWS_S3_KEY_VOCAB="input.txt"
+EPOCHS=$( printf "%04d" ${LEARNING_EPOCHS} )
+
+cd /app
+
+aws --region $AWS_DEFAULT_REGION s3api get-object \
+    --bucket $AWS_S3_BUCKET \
+    --key $AWS_S3_KEY_VOCAB $AWS_S3_KEY_VOCAB >/dev/null
+
+rc=$?
+if [ $rc -ne 0 ] ;then
+  echo "[Error] Receiving vocabluary file went wrong..." 1>&2
+  echo "AWS_S3_BUCKET: "${AWS_S3_BUCKET} 1>&2
+  echo "AWS_S3_KEY_VOCAB: "${AWS_S3_KEY_VOCAB} 1>&2
+  exit $rc
+fi
+
+python train.py
+
+rc=$?
+if [ $rc -ne 0 ] ;then
+  echo "[Error] Executing batch went wrong..." 1>&2
+  exit $rc
+fi
+
+aws --region $AWS_DEFAULT_REGION s3api put-object \
+    --bucket $AWS_S3_BUCKET --key "${AWS_S3_PREFIX}-${PARAMETERS_FILE}-${EPOCHS}.params" \
+    --body "${PARAMETERS_FILE}-${EPOCHS}.params" >/dev/null
+
+rc=$?
+if [ $rc -ne 0 ] ;then
+  echo "[Error] Sending job results (params) went wrong..." 1>&2
+  echo "AWS_S3_BUCKET: "${AWS_S3_BUCKET} 1>&2
+  echo "AWS_S3_KEY: ${AWS_S3_PREFIX}-${PARAMETERS_FILE}-${EPOCHS}.params" 1>&2
+  exit $rc
+fi
+
+aws --region $AWS_DEFAULT_REGION s3api put-object \
+    --bucket $AWS_S3_BUCKET --key "${AWS_S3_PREFIX}-${PARAMETERS_FILE}-symbol.json" \
+    --body "${PARAMETERS_FILE}-symbol.json" >/dev/null
+
+rc=$?
+if [ $rc -ne 0 ] ;then
+  echo "[Error] Sending job results (symbol) went wrong..." 1>&2
+  echo "AWS_S3_BUCKET: "${AWS_S3_BUCKET} 1>&2
+  echo "AWS_S3_KEY: ${AWS_S3_PREFIX}-${PARAMETERS_FILE}-symbol.json" 1>&2
+  exit $rc
+fi
